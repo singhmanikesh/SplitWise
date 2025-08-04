81efdf8bfce9a45ec9aca586a31fcf60197e917b
manikeshhhh <manikesh182003@gmail.com>
2025-08-04 12:46:12 +0530

    Added User Model

---
+++ src/main/java/com/manikesh/splitwise/splitwise/model/User.java
@@ -0,0 +1,23 @@
+package com.manikesh.splitwise.splitwise.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+    private String name;
+    private String email;
+}
